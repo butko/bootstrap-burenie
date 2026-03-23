c8c9ec24ead76c6fd8700f155979749b9b94f73c
butko <abutko@gmail.com>
2026-03-23 21:42:45 +0300

    рефакторинг

--- klyuch.php
+++ klyuch.php
@@ -1,98 +1,8 @@
 <!doctype html>
 <html lang="en" data-bs-theme="auto">
   <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="" />
-    <meta
-      name="author"
-      content="Mark Otto, Jacob Thornton, and Bootstrap contributors"
-    />
-    <meta name="generator" content="Astro v5.13.2" />
     <title>Бурение скважин под ключ</title>
-    <link
-      rel="canonical"
-      href="https://getbootstrap.com/docs/5.3/examples/carousel/"
-    />
-    <script src="assets/js/color-modes.js"></script>
-    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <meta name="theme-color" content="#712cf9" />
-    <link href="carousel.css" rel="stylesheet" />
-	<link href="custom.css" rel="stylesheet" />
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-      .b-example-divider {
-        width: 100%;
-        height: 3rem;
-        background-color: #0000001a;
-        border: solid rgba(0, 0, 0, 0.15);
-        border-width: 1px 0;
-        box-shadow:
-          inset 0 0.5em 1.5em #0000001a,
-          inset 0 0.125em 0.5em #00000026;
-      }
-      .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-      }
-      .bi {
-        vertical-align: -0.125em;
-        fill: currentColor;
-      }
-      .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-      }
-      .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-      }
-      .btn-bd-primary {
-        --bd-violet-bg: #712cf9;
-        --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
-        --bs-btn-font-weight: 600;
-        --bs-btn-color: var(--bs-white);
-        --bs-btn-bg: var(--bd-violet-bg);
-        --bs-btn-border-color: var(--bd-violet-bg);
-        --bs-btn-hover-color: var(--bs-white);
-        --bs-btn-hover-bg: #6528e0;
-        --bs-btn-hover-border-color: #6528e0;
-        --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
-        --bs-btn-active-color: var(--bs-btn-hover-color);
-        --bs-btn-active-bg: #5a23c8;
-        --bs-btn-active-border-color: #5a23c8;
-      }
-      .bd-mode-toggle {
-        z-index: 1500;
-      }
-      .bd-mode-toggle .bi {
-        width: 1em;
-        height: 1em;
-      }
-      .bd-mode-toggle .dropdown-menu .active .bi {
-        display: block !important;
-      }
-    </style>
+	<? include "head.inc" ?>
   </head>
   <body>
   
@@ -127,8 +37,8 @@
               <span class="text-body-secondary"></span>
 			  -->
             </h2>
-            <p class="lead">
-<p class="lead">Цена складывается из ряда параметров. Основные критерии:
+            
+<p class="lead">Цена складывается из ряда параметров. Основные критерии:</p>
 <ul>
 <li>глубина и вариант конструкции;
 <li>характеристики участка и грунта;
@@ -136,7 +46,25 @@
 <li>подбор оборудования.
 </ul>
 
-<p class="lead">В организацию системы водоснабжения под ключ входит:
+
+            
+          </div>
+          <div class="col-md-5 order-md-1">
+			<img src="img/klyuch2.jpg">
+          </div>
+        </div>
+        <hr class="featurette-divider" />
+		<a name="klyuch3">
+        <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading fw-normal lh-1">
+              Что входит?
+			  <!--
+              <span class="text-body-secondary">Checkmate.</span>
+			  -->
+            </h2>
+            
+<p class="lead">В организацию системы водоснабжения под ключ входит:</p>
 <ul>
 <li>Выезд инженера на участок
 <li>Составление проекта и расчет цены работы.
@@ -149,25 +77,8 @@
 <li>Обслуживание по гарантии.
 <li>Постгарантийный сервис.
 </ul>
-            </p>
-          </div>
-          <div class="col-md-5 order-md-1">
-			<img src="img/klyuch2.jpg">
-          </div>
-        </div>
-        <hr class="featurette-divider" />
-		<a name="klyuch3">
-        <div class="row featurette">
-          <div class="col-md-7">
-            <h2 class="featurette-heading fw-normal lh-1">
-              Канализация
-			  <!--
-              <span class="text-body-secondary">Checkmate.</span>
-			  -->
-            </h2>
-            <p class="lead">
-              Хотите сделать канализацию на даче? Для временного проживания подойдёт накопительный бак или простая выгребная яма. Для постоянного — лучше установить септик или биостанцию: они требуют меньше ухода.
-            </p>
+
+            
           </div>
           <div class="col-md-5">
 			<img src="img/klyuch3.jpg">
